6f069d34f78fe08f707ae8fed272265e4e381696
Mr.Sm1ley <Playpower101@gmail.com>
2023-08-22 15:13:50 +0200

    22.08 - all projects

---
+++ SampleHierarchies.Interfaces/Services/IScreenDefinitionService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleHierarchies.Interfaces.Services
+{
+    internal class IScreenDefinitionService
+    {
+    }
+}
